8f3577b3b1697c1628558f3ea7df9f685f984cf4
koushikkothagal <koush.s@gmail.com>
2013-08-03 21:07:59 -0700

    Cutomizing default names

--- src/org/koushik/javabrains/ProductCatalog.java
+++ src/org/koushik/javabrains/ProductCatalog.java
@@ -4,27 +4,30 @@ import java.util.List;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 
 import org.koushik.javabrains.business.ProductServiceImpl;
 
-@WebService
-public class ProductCatalog {
-	
-	ProductServiceImpl productService = new ProductServiceImpl();
 
-	@WebMethod
+@WebService(name="TestMartCatalog", portName="TestMartCatalogPort", serviceName="TestMartCatalogService", 
+			targetNamespace="http://www.testmart.com")
+public class ProductCatalog {
+
+	ProductServiceImpl productService = new ProductServiceImpl();
+	
+	@WebMethod(action="fetch_categories", operationName="fetchCategories")
 	public List<String> getProductCategories() {
 		return productService.getProductCategories();
 	}
 	
-	@WebMethod
+	@WebMethod(exclude=true)
 	public List<String> getProducts(String category) {
 		return productService.getProducts(category);
 	}
 	
-	@WebMethod
+	@WebMethod(exclude=true)
 	public boolean addProduct(String category, String product) {
 		return productService.addProduct(category, product);
 	}
-	
+
 }
